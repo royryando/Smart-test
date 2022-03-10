986479b07bc155448ec2be3d6ee30301fcc40e2e
maulayyacyber <ridaulmaulayya@gmail.com>
2022-03-10 21:27:02 +0700

    feat(add) : initial commit

---
+++ app/Models/Profit.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Profit extends Model
+{
+    use HasFactory;
+    
+    /**
+     * fillable
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'transaction_id', 'total'
+    ];
+
+    /**
+     * transaction
+     *
+     * @return void
+     */
+    public function transaction()
+    {
+        return $this->belongsTo(Transaction::class);
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+//prefix "apps"
+Route::prefix('apps')->group(function() {
+
+    //middleware "auth"
+    Route::group(['middleware' => ['auth']], function () {
+
+        //route dashboard
+        Route::get('dashboard', App\Http\Controllers\Apps\DashboardController::class)->name('apps.dashboard');
+        
+        //route permissions
+        Route::get('/permissions', \App\Http\Controllers\Apps\PermissionController::class)->name('apps.permissions.index')
+            ->middleware('permission:permissions.index');
+
+        //route resource roles
+        Route::resource('/roles', \App\Http\Controllers\Apps\RoleController::class, ['as' => 'apps'])
+            ->middleware('permission:roles.index|roles.create|roles.edit|roles.delete');
+
+        //route resource users
+        Route::resource('/users', \App\Http\Controllers\Apps\UserController::class, ['as' => 'apps'])
+            ->middleware('permission:users.index|users.create|users.edit|users.delete');
+
+    });
+});
